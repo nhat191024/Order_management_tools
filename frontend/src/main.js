b88b4b6fcc005edc0e9316075eb61c834bd04811
Fumino The Fish <richberchannel01@gmail.com>
2024-05-28 15:15:33 +0700

    Update: use route and import axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import router from './routers'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import axios from 'axios';
+window.axios = axios;
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');

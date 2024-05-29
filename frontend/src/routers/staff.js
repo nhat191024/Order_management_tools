ddaba70f0d105829da744b82b4bf06c8ad84007e
Quoc Anh <quocanhnguyen390@gmail.com>
2024-05-30 01:04:18 +0700

    touter select kitchen

--- frontend/src/routers/staff.js
+++ frontend/src/routers/staff.js
@@ -1,16 +1,20 @@
 const staff = [
-    {
+  {
+    path: "/",
+    component: () => import("../layouts/staff.vue"),
+    children: [
+      {
         path: "/",
-        component: () => import("../layouts/staff.vue"),
-        children: [
-            {
-                path: "/",
-                name: "staff",
-                component: () => import("../pages/staffs/index.vue"),
-            }
-        ]
-    }
-]
-
+        name: "staff",
+        component: () => import("../pages/staffs/index.vue"),
+      },
+      {
+        path: "/select_kitchen",
+        name: "chonBep",
+        component: () => import("../pages/staffs/select_kitchen.vue"),
+      },
+    ],
+  },
+];
 
 export default staff;

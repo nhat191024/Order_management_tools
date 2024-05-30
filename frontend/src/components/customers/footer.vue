8c7e515eb9e73c458c762fe741c96dd85d515969
VuongVu <vuongvu10899@gmail.com>
2024-05-30 14:10:18 +0700

    update footer

--- frontend/src/components/customers/footer.vue
+++ frontend/src/components/customers/footer.vue
@@ -1,18 +1,7 @@
 <template>
-  <nav class="bg-orange-500">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
-        <div
-          class="absolute inset-y-0 left-0 flex items-center sm:hidden"
-        ></div>
-        <div
-          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
-        >
-          <div class="flex flex-shrink-0 items-center">
-            <i class="fa-solid fa-cart-shopping text-white"></i>
-          </div>
-        </div>
-      </div>
+  <div class="flex bg-orange-500 h-20">
+    <div class="flex flex-row justify-center items-center mx-auto">
+      <img src="/src/assets/cart.svg" alt="Cart" class="text-white h-10" />
     </div>
-  </nav>
+  </div>
 </template>

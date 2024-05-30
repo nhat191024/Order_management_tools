d38164acce0a361b4e577d5e3902a21a92d6cbb4
VuongVu <vuongvu10899@gmail.com>
2024-05-30 15:28:07 +0700

    add nav and footer customer

--- frontend/src/components/customers/footer.vue
+++ frontend/src/components/customers/footer.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        this is customers footer
+  <div class="flex bg-orange-500 h-20">
+    <div class="flex flex-row justify-center items-center mx-auto">
+      <img src="/src/assets/cart.svg" alt="Cart" class="text-white h-10" />
     </div>
+  </div>
 </template>
+  

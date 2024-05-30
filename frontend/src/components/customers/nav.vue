2978294c25b89fe594ff53fe0b9c275e2f2ec137
VuongVu <vuongvu10899@gmail.com>
2024-05-30 14:10:29 +0700

    update nav

--- frontend/src/components/customers/nav.vue
+++ frontend/src/components/customers/nav.vue
@@ -1,22 +1,22 @@
 <template>
-  <nav class="bg-orange-500">
+  <nav class="bg-orange-500 h-30">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 items-center justify-between">
+      <div class="relative flex min-h-24 items-center justify-between">
         <div
-          class="absolute inset-y-0 left-2 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+          class="relative align-middle left-2 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
           <div class="flex flex-shrink-0">
             <img
-              class="h-8 w-auto rounded-full"
+              class="flex-row top-px rounded-full logo-size"
               src="/src/assets/logo.jpg"
               alt="Logo"
             />
           </div>
         </div>
         <div
-          class="absolute inset-y-0 right-2 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+          class="absolute right-2 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
         >
-          <p class="text-white">Bàn 1</p>
+          <p class="text-white text-2xl font-semibold">Bàn 1</p>
         </div>
       </div>
     </div>

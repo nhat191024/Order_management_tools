78d49db38f189928a0790457672548ad9f44ec55
Quoc Anh <quocanhnguyen390@gmail.com>
2024-05-31 20:23:45 +0700

    edit router food waiting

--- frontend/src/routers/staff.js
+++ frontend/src/routers/staff.js
@@ -1,16 +1,20 @@
 const staff = [
-    {
+  {
+    path: "/",
+    component: () => import("../layouts/staff.vue"),
+    children: [
+      {
         path: "/",
-        component: () => import("../layouts/staff.vue"),
-        children: [
-            {
-                path: "/",
-                name: "staff",
-                component: () => import("../pages/staffs/index.vue"),
-            }
-        ]
-    }
-]
-
+        name: "staff",
+        component: () => import("../pages/staffs/index.vue"),
+      },
+      {
+        path: "/show_food_waiting",
+        name: "WaitFood",
+        component: () => import("../pages/staffs/show_food_waiting.vue"),
+      },
+    ],
+  },
+];
 
 export default staff;

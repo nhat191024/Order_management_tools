33a005f829c5430a8fe269eb46224e6b07ce17e5
FuminoLap <richberchannel01@gmail.com>
2024-06-05 17:05:10 +0700

    Add: router for select tablet

--- frontend/src/routers/staff.js
+++ frontend/src/routers/staff.js
@@ -9,9 +9,14 @@ const staff = [
         component: () => import("../pages/staffs/index.vue"),
       },
       {
-        path: "/select_kitchen",
-        name: "chonBep",
-        component: () => import("../pages/staffs/select_kitchen.vue"),
+        path: "/selectTable",
+        name: "select_table",
+        component: () => import("../pages/staffs/selectTable.vue"),
+      },
+      {
+        path: "/selectKitchen",
+        name: "select_kitchen",
+        component: () => import("../pages/staffs/selectKitchen.vue"),
       },
     ],
   },

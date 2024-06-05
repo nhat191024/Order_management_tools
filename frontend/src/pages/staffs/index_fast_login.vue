ab72148e1fc7de46cb69407b199fee476e9cfa29
Fumino The Fish <richberchannel01@gmail.com>
2024-06-05 11:31:05 +0700

    Create: second login screen

---
+++ frontend/src/pages/staffs/index_fast_login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="grid grid-cols-12 grid-rows-10 gap-5 h-screen w-screen">
+        <p class="col-start-2 col-span-4 row-start-2 row-auto self-center font-extrabold text-black text-4xl">
+            Đăng Nhập Nhanh
+        </p>
+        <div class="col-start-2 col-span-4 row-start-3 row-auto self-end bg-orange-500 py-3 pl-5 rounded-full flex items-center gap-6">
+            <img src="../../assets/logo.jpg" alt="" class="rounded-full w-1/6">
+            <p class="font-bold text-white text-xl">Nguyễn Thị Nở</p>
+        </div>
+        <Form
+            class="col-start-8 col-span-4 row-start-2 row-span-8 border border-solid bg-orange-500 rounded-xl grid grid-cols-3 grid-rows-10 gap-2">
+            <img src="../../assets/logo.jpg" alt="logo" class=" rounded-full col-start-2 row-span-4 self-center">
+            <Field type="text" name="username" placeholder="Tên Đăng Nhập"
+                class="row-start-5 col-span-3 mx-10 mb-1 -mt-2 rounded-2xl text-center bg-white drop-shadow-2xl" />
+            <Field type="password" name="password" placeholder="Mật Khẩu"
+                class="row-start-6 col-span-3 mx-10 mt-1 -mb-2 rounded-2xl text-center bg-white drop-shadow-2xl" />
+            <button type="submit"
+                class="btn bg-orange-400 border-0 drop-shadow-lg text-white row-start-8 col-start-1 col-span-3 self-center mx-10">Đăng
+                Nhập</button>
+        </Form>
+    </div>
+</template>
+
+<script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+</script>

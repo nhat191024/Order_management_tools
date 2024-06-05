56888a30622c6aee8f913a2d1b40b22b75104d17
FuminoLap <richberchannel01@gmail.com>
2024-06-05 16:39:12 +0700

    Update: change form layout + add validation

--- frontend/src/pages/staffs/index.vue
+++ frontend/src/pages/staffs/index.vue
@@ -1,21 +1,38 @@
 <template>
     <div class="grid grid-cols-12 grid-rows-10 gap-5 h-screen w-screen">
-        <Form
-            class="col-start-5 col-span-4 row-start-2 row-span-8 border border-solid bg-orange-500 rounded-xl grid grid-cols-3 grid-rows-10 gap-2">
-            <img src="../../assets/logo.jpg" alt="logo" class=" rounded-full col-start-2 row-span-4 self-center">
+        <Form @submit="onSubmit"
+            class="col-start-5 col-span-4 row-start-2 row-span-8 border border-solid bg-orange-500 rounded-xl grid grid-cols-3 grid-rows-7 px-10">
+            <img src="../../assets/logo.jpg" alt="logo"
+                class=" rounded-full col-span-full row-span-3 self-center place-self-center w-[50%]">
             <Field type="text" name="username" placeholder="Tên Đăng Nhập"
-                class="row-start-5 col-span-3 mx-10 mb-1 -mt-2 rounded-2xl text-center bg-white drop-shadow-2xl" />
+                class="row-start-4 col-span-full h-[60%] rounded-2xl text-center bg-white drop-shadow-2xl"
+                :rules="usernameRule" />
+            <ErrorMessage class="col-span-full row-start-4 self-end mb-1 place-self-center font-bold text-white"
+                name="username" />
             <Field type="password" name="password" placeholder="Mật Khẩu"
-                class="row-start-6 col-span-3 mx-10 mt-1 -mb-2 rounded-2xl text-center bg-white drop-shadow-2xl" />
+                class="row-start-5 col-span-full  h-[60%] rounded-2xl text-center bg-white drop-shadow-2xl"
+                :rules="passwordRule" />
+            <ErrorMessage class="col-span-full row-start-5 self-end mb-1 place-self-center text-white font-bold"
+                name="password" />
             <button type="submit"
-                class="btn bg-orange-400 border-0 drop-shadow-lg text-white row-start-8 col-start-1 col-span-3 self-center mx-10">Đăng
-                Nhập</button>
+                class="btn bg-orange-400 border-0 drop-shadow-lg text-white row-start-6 row-span-2 col-span-3 self-center">
+                Đăng Nhập
+            </button>
         </Form>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import * as yup from 'yup';
 
+const usernameRule = yup.string().required('Tên Đăng Nhập là bắt buộc');
+const passwordRule = yup.string().required('Mật khẩu là bắt buộc').min(6, 'Mật khẩu phải có ít nhất 6 ký tự');
+
+const auth = ref('none');
+
+function onSubmit(values) {
+
+}
 </script>

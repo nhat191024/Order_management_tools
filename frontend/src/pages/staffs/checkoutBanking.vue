444c124f22aa1b718a0cd148f6d4fa7986e5abe0
mlemingcapoo <ducvu73@gmail.com>
2024-06-07 00:28:46 +0700

    ADD: Init template file - checkout with banking app

---
+++ frontend/src/pages/staffs/checkoutBanking.vue
@@ -0,0 +1,20 @@
+<template>
+</template>
+<div class="grid grid-cols-12 grid-rows-8 gap-5 h-screen w-screen ">
+
+</div>
+<script>
+export default {
+    data() {
+        return {
+            testVar: 'test',
+        }
+    },
+
+    methods: {
+        test() {
+            console.log("test")
+        }
+    }
+}
+</script>

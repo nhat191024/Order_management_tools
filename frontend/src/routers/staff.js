b63178a2052caa29c463a8791517469b21739ff4
mlemingcapoo <ducvu73@gmail.com>
2024-06-07 00:29:37 +0700

    ADD: Add 2 new routes for Bill Checkout methods

--- frontend/src/routers/staff.js
+++ frontend/src/routers/staff.js
@@ -13,6 +13,16 @@ const staff = [
                         component: () => import("../pages/staffs/index.vue"),
                     },
                     {
+                        path: "staff/checkout/banking",
+                        name: "staff_checkout_banking",
+                        component: () => import("../pages/staffs/checkoutBanking.vue"),
+                    },
+                    {
+                        path: "staff/checkout/cash",
+                        name: "staff_checkout_cash",
+                        component: () => import("../pages/staffs/checkoutCash.vue"),
+                    },
+                    {
                         path: "/kitchen/selectTable",
                         name: "kitchen_select_Table",
                         component: () => import("../pages/staffs/selectTable.vue"),

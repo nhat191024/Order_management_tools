34d15811b8dd6810591be0309102ae85cd1842db
Fumino The Fish <richberchannel01@gmail.com>
2024-06-06 22:55:33 +0700

    Update: theme color config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,9 +5,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF7A00',
+      }
+    },
   },
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        orange: {
+          primary: '#FFA14A'
+        }
+      }
+    ]
+  }
 }

36b1c286f3beb514afb195d2614e18667d1f3b9c
mlemingcapoo <ducvu73@gmail.com>
2024-06-07 01:39:24 +0700

    ADD: Thêm thành phần bill (chỉ hoá đơn) để sử dụng lại

---
+++ frontend/src/components/staffs/bill.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="border-1 shadow-md bg-white rounded-xl w-max h-max">
+    <span class="text-center flex">Hoá đơn thanh toán</span>
+    <table class="table m-1">
+        <thead>
+            <tr>
+                <th>Tên Món</th>
+                <th>Số lượng</th>
+                <th>Đơn Giá</th>
+                <th>Thành Tiền</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="i in billData.items" :key="i.name">
+                <td>{{ i.name }}</td>
+                <td>{{ i.quantity }}</td>
+                <td>{{ i.price }}</td>
+                <td>{{ i.price * i.quantity }}</td>
+            </tr>
+        </tbody>
+    </table>
+    <span class="text-center flex">Tổng thanh toán: {{ billData.total(billData.items) }} VND</span>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        billData: Object,
+    }
+};
+</script>

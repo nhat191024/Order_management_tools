1d5d303842180e221ff8810eda893e60407435f7
FuminoLap <richberchannel01@gmail.com>
2024-06-08 13:48:55 +0700

    Fix: resolve conflicts

--- frontend/src/routers/staff.js
+++ frontend/src/routers/staff.js
@@ -5,16 +5,31 @@ const staff = [
     children: [
       {
         path: "/",
-        name: "staff",
-        component: () => import("../pages/staffs/index.vue"),
-      },
-      {
-        path: "/select_kitchen",
-        name: "chonBep",
-        component: () => import("../pages/staffs/select_kitchen.vue"),
-      },
-    ],
-  },
-];
-
+        component: () => import("../layouts/staff.vue"),
+        children: [
+          {
+            path: "/staff/login",
+            name: "login",
+            component: () => import("../pages/staffs/index.vue"),
+          },
+          {
+            path: "/staff/table",
+            name: "table selection",
+            component: () => import("../pages/staffs/selectTable.vue"),
+          },
+          {
+            path: "/staff/table/:id",
+            name: "table detail",
+            component: () => import("../pages/staffs/tableDetail.vue"),
+          },
+          {
+            path: "/kitchen/select",
+            name: "kitchen selection",
+            component: () => import("../pages/staffs/selectKitchen.vue"),
+          },
+        ]
+      }
+    ]
+  }
+]
 export default staff;

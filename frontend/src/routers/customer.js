9ddb420162858df5786ad5ef7de51fa61a1cc80d
huylee47 <huyw300@gmail.com>
2024-06-09 11:49:43 +0700

    update routers customer

--- frontend/src/routers/customer.js
+++ frontend/src/routers/customer.js
@@ -7,10 +7,19 @@ const customer = [
                 path: "/",
                 name: "order",
                 component: () => import("../pages/customers/index.vue"),
-            }
+            },
         ]
+    },
+    {
+        path: "/payment",
+        name: "payment",
+        component: () => import("../pages/customers/pay.vue"),
     }
 ]
 
-
+// {
+//     path: "/payment",
+//     name: "payment",
+//     component: () => import("../pages/customers/payment.vue"),
+// }
 export default customer;

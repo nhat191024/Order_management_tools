bbef3a650c9d698ef43c4012207a9fed01d09b7e
huylee47 <huyw300@gmail.com>
2024-06-09 21:32:02 +0700

    Update: Order Comfirm (nav/footer)

---
+++ frontend/src/pages/customers/orderConfirm.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="h-dvh w-dvw grid grid-row-12">
+  <nav class="bg-primary  top-0 left-0 w-full row-span-1 flex items-center justify-between px-4 rounded-b-lg">
+    <img class="rounded-full w-14 h-14" src="/src/assets/backButton.svg" alt="Logo" />
+    <p class="text-white text-2xl font-semibold">Bàn {{ }}</p>
+  </nav>
+
+  <div class="">
+    <div class="overflow-y-auto h-80 ">
+      <div class="flex-auto my-6">
+        <p class="text-black text-2xl font-semibold text-center">Xác nhận đơn hàng</p>
+      </div>
+      <hr class="my-5" />
+      <div class="flex ...">
+        <div class="flex-none w-14 h-14 ms-1">
+          <img src="/src/assets/logo.jpg" alt="" width="100%" class="rounded-lg" />
+        </div>
+        <div class="grow h-14 ...">
+          <div class="grid grid-cols-3">
+            <span class="ms-3">1x</span>
+            <span class="">Tên món</span>
+            <div>
+              <span class="float-right me-2">80.000Đ</span>
+            </div>
+            <div dir="ltr">
+              <span class="ms-3">Ghi chú</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-5" />
+      <div class="flex ...">
+        <div class="flex-none w-14 h-14 ms-1">
+          <img src="/src/assets/logo.jpg" alt="" width="100%" class="rounded-lg" />
+        </div>
+        <div class="grow h-14 ...">
+          <div class="grid grid-cols-3">
+            <span class="ms-3">1x</span>
+            <span class="">Tên món</span>
+            <div>
+              <span class="float-right me-2">80.000Đ</span>
+            </div>
+            <div dir="ltr">
+              <span class="ms-3">Ghi chú</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-5" />
+      <div class="flex ...">
+        <div class="flex-none w-14 h-14 ms-1">
+          <img src="/src/assets/logo.jpg" alt="" width="100%" class="rounded-lg" />
+        </div>
+        <div class="grow h-14 ...">
+          <div class="grid grid-cols-3">
+            <span class="ms-3">1x</span>
+            <span class="">Tên món</span>
+            <div>
+              <span class="float-right me-2">80.000Đ</span>
+            </div>
+            <div dir="ltr">
+              <span class="ms-3">Ghi chú</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-5" />
+      <div class="flex ...">
+        <div class="flex-none w-14 h-14 ms-1">
+          <img src="/src/assets/logo.jpg" alt="" width="100%" class="rounded-lg" />
+        </div>
+        <div class="grow h-14 ...">
+          <div class="grid grid-cols-3">
+            <span class="ms-3">1x</span>
+            <span class="">Tên món</span>
+            <div>
+              <span class="float-right me-2">80.000Đ</span>
+            </div>
+            <div dir="ltr">
+              <span class="ms-3">Ghi chú</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-5" />
+      <div class="flex ...">
+        <div class="flex-none w-14 h-14 ms-1">
+          <img src="/src/assets/logo.jpg" alt="" width="100%" class="rounded-lg" />
+        </div>
+        <div class="grow h-14 ...">
+          <div class="grid grid-cols-3">
+            <span class="ms-3">1x</span>
+            <span class="">Tên món</span>
+            <div>
+              <span class="float-right me-2">80.000Đ</span>
+            </div>
+            <div dir="ltr">
+              <span class="ms-3">Ghi chú</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-5" />
+      <div class="flex ...">
+        <div class="flex-none w-14 h-14 ms-1">
+          <img src="/src/assets/logo.jpg" alt="" width="100%" class="rounded-lg" />
+        </div>
+        <div class="grow h-14 ...">
+          <div class="grid grid-cols-3">
+            <span class="ms-3">1x</span>
+            <span class="">Tên món</span>
+            <div>
+              <span class="float-right me-2">80.000Đ</span>
+            </div>
+            <div dir="ltr">
+              <span class="ms-3">Ghi chú</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="container mx-auto verflow-auto">
+    <hr class="my-1" />
+    <div class="my-5 o">
+      <span class="text-3xl">Tổng cộng :</span>
+      <span class="float-right me-2 text-3xl">
+        <p style="color: #ff5c00">170.000Đ</p>
+      </span>
+    </div>
+    <div class="grid grid-cols-1 m-2 my-5">
+      <button class="btn btn-primary">
+        <p style="color: white">Thanh toán</p>
+      </button>
+    </div>
+  </footer>
+</div>
+</template>

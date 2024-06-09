37eccae0eae9acbde9c227b5f054d65860d96522
Quoc Anh <quocanhnguyen390@gmail.com>
2024-06-09 12:22:17 +0700

    Add: showFoodWaiting

---
+++ frontend/src/pages/staffs/showFoodWaiting.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="h-screen w-screen overflow-scroll">
+    <div class="fixed w-full bg-white shadow-md">
+      <div class="flex items-center p-4">
+        <div class="flex items-center justify-center col-span-2">
+          <button class="flex items-center text-black bg-white drop-shadow-2x">
+            <div
+              class="flex items-center justify-center w-10 h-10 m-2 bg-primary rounded-xl drop-shadow-2xl p-1"
+            >
+              <img src="./../../assets/left-long-solid.svg" alt="My SVG Icon" />
+            </div>
+            <span class="text-xl drop-shadow">Trở về</span>
+          </button>
+        </div>
+        <h1 class="flex-grow text-5xl font-bold text-center text-black">
+          Bếp xào
+        </h1>
+        <div class="w-20"></div>
+      </div>
+      <hr class="mt-3" />
+    </div>
+
+    <div
+      class="grid gap-4 p-4 mt-20 place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+    >
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="flex flex-col items-center p-4 m-2 text-center text-white bg-primary rounded-lg shadow-lg w-64 cursor-pointer"
+        @click="toggleItem(index)"
+      >
+        <template v-if="item.visible">
+          <div class="text-lg font-bold">{{ item.title }}</div>
+          <div class="w-full my-2 border-t border-white"></div>
+          <div class="text-lg">{{ item.description }}</div>
+          <div class="w-full my-2 border-t border-white"></div>
+          <div class="flex items-center justify-between w-full space-x-2">
+            <div class="text-xl font-bold">{{ item.table }}</div>
+            <div
+              class="flex items-center justify-center w-10 h-10 p-2 bg-white rounded-full"
+            >
+              <img src="./../../assets/check.svg" alt="Check Icon" />
+            </div>
+          </div>
+        </template>
+        <template v-else>
+          <div class="text-lg font-bold">Thẻ mới</div>
+          <div class="w-full my-2 border-t border-white"></div>
+          <div class="text-lg">Nội dung mới</div>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 1",
+    visible: true,
+  },
+  {
+    title: "Ốc bươu xào",
+    description: "Không nướng",
+    table: "Bàn 2",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 3",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 1",
+    visible: true,
+  },
+  {
+    title: "Ốc bươu xào",
+    description: "Không nướng",
+    table: "Bàn 2",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 3",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 1",
+    visible: true,
+  },
+  {
+    title: "Ốc bươu xào",
+    description: "Không nướng",
+    table: "Bàn 2",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 3",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 1",
+    visible: true,
+  },
+  {
+    title: "Ốc bươu xào",
+    description: "Không nướng",
+    table: "Bàn 2",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 3",
+    visible: true,
+  },
+  {
+    title: "Ốc sên xào",
+    description: "Không nướng",
+    table: "Bàn 1",
+    visible: true,
+  },
+  {
+    title: "Ốc bươu xào",
+    description: "Không nướng",
+    table: "Bàn 2",
+    visible: true,
+  },
+]);
+
+const toggleItem = (index) => {
+  items.value[index].visible = !items.value[index].visible;
+};
+</script>

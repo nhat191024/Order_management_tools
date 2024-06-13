6c1c45046db7f22a9d84ccd5bd1d75c40ea32551
mlemingcapoo <ducvu73@gmail.com>
2024-06-13 19:03:32 +0700

    Add: Bill controller

---
+++ backend/app/Http/Controllers/BillController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BillCollection;
+use App\Http\Resources\BillResource;
+use App\Models\Bill;
+use Illuminate\Http\Request;
+
+class BillController extends Controller
+{
+
+    public function index()
+    {
+        return Bill::all();
+    }
+
+    public function show($id){
+        $bill = Bill::where('id', '=', '1');
+        $bill = $bill->with('table','billDetail','billDetail.dish')->get();
+        return new BillCollection($bill);
+    }
+
+
+
+
+}

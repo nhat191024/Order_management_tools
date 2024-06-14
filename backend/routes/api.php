7822cb0327ce7016fd1e024363a0d203746415a1
Fumino The Fish <richberchannel01@gmail.com>
2024-06-15 02:56:57 +0700

    Update: login route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\DishController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/login', [UserController::class, 'login']);

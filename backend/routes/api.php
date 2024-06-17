b09bf9f9ac6c55df756b53595a8c0e07c4a8e443
Fumino The Fish <richberchannel01@gmail.com>
2024-06-17 10:10:38 +0700

    Cut: test controller call

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\DishController;
 
 /*
 |--------------------------------------------------------------------------

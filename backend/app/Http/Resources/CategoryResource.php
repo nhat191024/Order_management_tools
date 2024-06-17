6e840cd6d97fd4062d16735caa3e83c19b52f0ff
Fumino The Fish <richberchannel01@gmail.com>
2024-06-17 21:19:40 +0700

    Update: rename file

--- backend/app/Http/Resources/CategoryResource.php
+++ backend/app/Http/Resources/CategoryResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class MenuResource extends JsonResource
+class CategoryResource extends JsonResource
 {
     /**
      * Transform the resource into an array.

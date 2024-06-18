ddd476b13787f27a774702122eb2752365338167
Fumino The Fish <richberchannel01@gmail.com>
2024-06-18 21:13:58 +0700

    Refactor

--- backend/app/Http/Resources/tableCollection.php
+++ backend/app/Http/Resources/tableCollection.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class tableCollection extends ResourceCollection
+class TableCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.

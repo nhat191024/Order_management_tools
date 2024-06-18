6c2abc41cacb56ce02462c12fdc928da1e31d57a
Fumino The Fish <richberchannel01@gmail.com>
2024-06-18 11:19:24 +0700

    Update: code comment for someone who need it

--- backend/app/Service/TableDetailManagerService.php
+++ backend/app/Service/TableDetailManagerService.php
@@ -18,8 +18,11 @@ class TableDetailManagerService
     {
         $bills = Bill::where('id', ">", "0");
         $bills = $bills->with('billDetail', 'billDetail.dish', 'billDetail.dish.food', 'billDetail.dish.cookingMethod', 'table', 'table.branch')->get();
+
+        // Calculate total price of each bill
         foreach ($bills as $bill) {
             $total = 0;
+            // Calculate total price of each dish in the bill
             foreach ($bill->billDetail as $billDetail) {
                 $billDetail->price = $billDetail->dish->food->price + $billDetail->dish->additional_price;
                 $total += $billDetail->price * $billDetail->quantity;

f258775795e0fc1df1d588501b506662a9cb206a
Fumino The Fish <richberchannel01@gmail.com>
2024-06-18 11:13:15 +0700

    Update: branch resource

--- backend/app/Http/Resources/BranchResource.php
+++ backend/app/Http/Resources/BranchResource.php
@@ -14,6 +14,9 @@ class BranchResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'Branch_id' => $this->id,
+            'Branch_name' => $this->name,
+        ];
     }
 }

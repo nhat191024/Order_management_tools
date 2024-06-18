0e52465b6e39004a64490c2cc2e6f2f6fb5331bb
Fumino The Fish <richberchannel01@gmail.com>
2024-06-18 11:10:31 +0700

    Update: get table current bill function

--- backend/app/Service/TableDetailManagerService.php
+++ backend/app/Service/TableDetailManagerService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Models\Category;
+use App\Models\Bill;
 
 class TableDetailManagerService
 {
@@ -11,5 +12,20 @@ class TableDetailManagerService
         $menu = Category::where('id', ">", "0");
         $menu = $menu->with('food', 'food.dish', 'food.dish.cookingMethod')->get();
         return $menu;
+    }
+
+    public function getTableCurrentBill()
+    {
+        $bills = Bill::where('id', ">", "0");
+        $bills = $bills->with('billDetail', 'billDetail.dish', 'billDetail.dish.food', 'billDetail.dish.cookingMethod', 'table', 'table.branch')->get();
+        foreach ($bills as $bill) {
+            $total = 0;
+            foreach ($bill->billDetail as $billDetail) {
+                $billDetail->price = $billDetail->dish->food->price + $billDetail->dish->additional_price;
+                $total += $billDetail->price * $billDetail->quantity;
+            }
+            $bill->total = $total;
+        }
+        return $bills;
     }
 }

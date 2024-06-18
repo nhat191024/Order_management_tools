737cfba78bc03530adb6d1c039af01de042f9c36
mlemingcapoo <ducvu73@gmail.com>
2024-06-18 23:01:43 +0700

    Update: Routes to billCheckout Api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BillController;
+use App\Http\Controllers\Api\BillCheckoutController;
 
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\TableDetailManagerController;
@@ -23,4 +23,5 @@ Route::group(['prefix' => 'staff', 'namespace' => 'App\Http\Controllers\Api', 'm
     Route::get('/menu', [TableDetailManagerController::class, 'menu']);
     Route::get('/currentOrder', [TableDetailManagerController::class, 'currentOrder']);
     Route::post('/order', [TableDetailManagerController::class, 'order']);
+    Route::get('/billCheckout/{id}',[BillCheckoutController::class,'show']);
 });

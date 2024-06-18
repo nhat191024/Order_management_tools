f33f40b1d381614cb23c5b7de69ef038a6afc47c
mlemingcapoo <ducvu73@gmail.com>
2024-06-18 21:28:33 +0700

    Fix: Remove duplicate controller

--- backend/app/Http/Controllers/BillController.php
+++ backend/app/Http/Controllers/BillController.php
@@ -17,34 +17,6 @@ class BillController extends Controller
 
     public function show($id){
         $bill = Bill::where('id', '=', '1');
-        $bill = $bill->with('table','billDetail','billDetail.dish', 'billDetail.dish.food')->get();
-        return new BillCollection($bill);
-    }
-
-
-
-
-}
-
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Http\Resources\BillCollection;
-use App\Http\Resources\BillResource;
-use App\Models\Bill;
-use Illuminate\Http\Request;
-
-class BillController extends Controller
-{
-
-    public function index()
-    {
-        return Bill::all();
-    }
-
-    public function show($id){
-        $bill = Bill::where('id', '=', '1');
         $bill = $bill->with('table','billDetail','billDetail.dish')->get();
         return new BillCollection($bill);
     }

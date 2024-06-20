c42839c2ad4c995837cfffdf2505bf54afb23913
Fumino The Fish <richberchannel01@gmail.com>
2024-06-20 22:12:25 +0700

    Refactor

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\CustomerController;
+
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\TableDetailManagerController;
+use App\Http\Controllers\Api\CustomerController;
 
 /*
 |--------------------------------------------------------------------------

efbd2c7e8074d864ee12f0279f11cabd51c86866
mlemingcapoo <ducvu73@gmail.com>
2024-06-21 01:47:52 +0700

    Update: adding export modifier

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const bearerToken = '1|GCIMjkZDfpH8K95NvRV5qervTAAuIxjmgr14icnt51cc86c6';
 
 console.log(bearerToken);
-const api = axios.create({
+export const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/',
     headers: {
         Authorization: `Bearer ${bearerToken}`,

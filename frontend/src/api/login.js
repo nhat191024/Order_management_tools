d1fd248eac1c99ea2cca22956adc1852b4f432f6
Fumino The Fish <richberchannel01@gmail.com>
2024-06-21 14:49:12 +0700

    Update: cookies expires time now will be 1 day, add return message, add cookies delete when logout for token and branch

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -3,7 +3,7 @@ import { api } from "./api";
 export async function loginHandle(username, password) {
     try {
         let date = new Date();
-        const expires = new Date(date.getTime() + 2 * 60 * 60 * 1000);
+        const expires = new Date(date.getTime() + 24 * 60 * 60 * 1000);
         const response = await api.post('/login', {
             username: username,
             password: password
@@ -14,11 +14,16 @@ export async function loginHandle(username, password) {
             document.cookie = `Role=${response.data.data.role}; expires=${expires.toUTCString}; path=/`;
             document.cookie = `Id=${response.data.data.id}; expires=${expires.toUTCString}; path=/`;
             document.cookie = `Branch_id=${response.data.data.branch_id}; expires=${expires.toUTCString}; path=/`;
-            return 'success';
+            return {
+                message: 'success',
+                role: response.data.data.role
+            };
         }
     } catch (err) {
         if (err.response.status === 401) {
-            return 'fail';
+            return {
+                message: 'fail'
+            };
         }
     }
 }
@@ -27,9 +32,10 @@ export async function logout() {
     try {
         const response = await api.post('/logout');
         if (response.data.Message == 'logout success') {
-            localStorage.removeItem('Token');
+            document.cookie = `Token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
             document.cookie = `Role=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
             document.cookie = `Id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+            document.cookie = `Branch_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
             return 'success';
         }
     } catch (err) { }

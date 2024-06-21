8cc9166aa15eb3fd2c9ee37f8881022e4883e961
Fumino The Fish <richberchannel01@gmail.com>
2024-06-21 14:47:56 +0700

    Update: now token will take from cookies

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
-// const bearerToken = '1|PyARy0nGVMtmeWflwhQitXYTPrAGcGuffliLhiV00bc1a92e';
+const token = document.cookie.split(';').find(cookie => cookie.includes('Token')).split('=')[1];
 
-console.log(bearerToken);
 export const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/',
     headers: {
-        Authorization: `Bearer ${bearerToken}`,
+        Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',
         'Accept': 'application/json',
     },

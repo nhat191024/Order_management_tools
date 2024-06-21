a5c9812395051bd8f289f19183afef405c3d056d
Fumino The Fish <richberchannel01@gmail.com>
2024-06-22 04:38:00 +0700

    Update: get cookie function

--- frontend/src/api/functions.js
+++ frontend/src/api/functions.js
@@ -12,3 +12,20 @@ export function formatPrice(price) {
     if (price == null) return;
     return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
+
+export function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}

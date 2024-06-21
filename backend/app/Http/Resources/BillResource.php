a715e502821152c5cd846d38a378cf309bec1ff2
Fumino The Fish <richberchannel01@gmail.com>
2024-06-21 17:23:23 +0700

    Update: add time format

--- backend/app/Http/Resources/BillResource.php
+++ backend/app/Http/Resources/BillResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class BillResource extends JsonResource
 {
@@ -20,8 +21,8 @@ class BillResource extends JsonResource
             'Bill_user' => new UserResource($this->whenLoaded('user')),
             'Bill_total' => $this->total,
             'Bill_pay_status' => $this->pay_status,
-            'Bill_time_in' => $this->time_in,
-            'Bill_time_out' => $this->time_out,
+            'Bill_time_in' => Carbon::parse($this->time_in)->format('H:i:s d:m:Y'),
+            'Bill_time_out' => Carbon::parse($this->time_out)->format('H:i:s d:m:Y'),
             'Bill_detail' => new BillDetailCollection($this->whenLoaded('billDetail')),
         ];
     }

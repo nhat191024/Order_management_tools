defb3c8ed5e62f82e67b9119e2d4069f68d9db0f
Fumino The Fish <richberchannel01@gmail.com>
2024-06-22 04:37:46 +0700

    Update: add order function

--- frontend/src/api/tableDetail.js
+++ frontend/src/api/tableDetail.js
@@ -6,8 +6,18 @@ export async function getMenu() {
     });
 }
 
-export async function getTableCurrentBill() {
-    return await api.get(`/staff/currentOrder`).then((res) => {
-        return res.data.data;
+export async function getTableCurrentBill(id) {
+    return api.get(`/staff/currentOrder/${id}`).then((res) => {
+        return res.data.data[0].Table_bill;
     });
 }
+
+export async function addOrderItems(table_id, user_id, ...dishes) {
+    return await api.post(`/staff/order`, {
+        table_id: table_id,
+        user_id: user_id,
+        dishes: dishes
+    }).then((res) => {
+        return res.data.data;
+    });
+} 

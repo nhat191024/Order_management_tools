d15061594f29102c807f99720e55ef598c1b2e44
Fumino The Fish <richberchannel01@gmail.com>
2024-06-22 19:42:24 +0700

    Update: add error catch

--- frontend/src/api/table.js
+++ frontend/src/api/table.js
@@ -1,7 +1,11 @@
 import { api } from './api';
 
-export async function getTableData(id){
-    return await api.get(`staff/tableList/${id}`).then((res)=>{
-        return res.data.data;
-    });
+export async function getTableData(id) {
+    try {
+        return await api.get(`staff/tableList/${id}`).then((res) => {
+            return res.data.data;
+        });
+    } catch (error) {
+        window.location.reload();
+    }
 }

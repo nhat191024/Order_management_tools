697b87172956e9ea269817ecf28bedc35f2424d7
Fumino The Fish <richberchannel01@gmail.com>
2024-06-23 20:18:12 +0700

    Remove: test

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const token = document.cookie.split(';').find(cookie => cookie.includes('Token')).split('=')[1];
-const token = "";
+const token = document.cookie.split(';').find(cookie => cookie.includes('Token')).split('=')[1];
 
 export const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",

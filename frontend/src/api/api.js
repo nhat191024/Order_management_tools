bcc3cdd21dc425c9fd24ed41cac8cbfaecc34a7e
Fumino The Fish <richberchannel01@gmail.com>
2024-06-26 18:26:10 +0700

    Update: change url to test server url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import { getCookie } from './functions';
 const token = getCookie('Token');
 
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: 'http://van191024.xyz/api/',
     headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json',

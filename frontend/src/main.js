24c8972d2c80aa114bc153603ef0b0344c9638be
Fumino The Fish <richberchannel01@gmail.com>
2024-06-27 10:48:52 +0700

    Update: import pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import router from './routers'
 import './style.css'
 import App from './App.vue'
@@ -7,5 +8,7 @@ import axios from 'axios';
 window.axios = axios;
 
 const app = createApp(App);
-app.use(router);
+const pinia = createPinia();
+
+app.use(router, pinia);
 app.mount('#app');

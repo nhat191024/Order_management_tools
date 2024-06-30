6f83a02caee4ceec57e210ff800b0614c9160394
Fumino The Fish <richberchannel01@gmail.com>
2024-06-30 19:57:36 +0700

    Update: import pusher and laravel echo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,20 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './routers'
-import './style.css'
-import App from './App.vue'
-
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './routers';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js/with-encryption';
+import App from './App.vue';
 import axios from 'axios';
+import './style.css';
+
+window.Pusher = Pusher;
 window.axios = axios;
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    encrypted: true,
+});
 
 const app = createApp(App);
 const pinia = createPinia();

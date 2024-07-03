f937dc6267ecf31e8849349df3bc7ab87e76bf2a
Fumino The Fish <richberchannel01@gmail.com>
2024-07-03 18:20:19 +0700

    Update: add bill branch id

--- backend/database/migrations/2024_06_10_133118_bills.php
+++ backend/database/migrations/2024_06_10_133118_bills.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bills', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('table_id');
+            $table->unsignedBigInteger('branch_id');
             $table->unsignedBigInteger('user_id')->nullable();
             $table->timestamp('time_in')->useCurrent();
             $table->timestamp('time_out')->nullable();

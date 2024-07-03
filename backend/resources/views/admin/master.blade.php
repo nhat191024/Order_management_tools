be649564501385585331a905e073d05ff558ff41
lamtnk2 <lamtnk2@fpt.edu.vn>
2024-07-04 01:56:20 +0700

    add scroll to top

--- backend/resources/views/admin/master.blade.php
+++ backend/resources/views/admin/master.blade.php
@@ -413,7 +413,7 @@
 
             </div>
             <!-- Scroll to Top Button-->
-            <a class="scroll-to-top rounded" href="{{ url('') . '/' }}#page-top">
+            <a class="scroll-to-top rounded" href="#page-top">
                 <i class="fas fa-angle-up"></i>
             </a>
 

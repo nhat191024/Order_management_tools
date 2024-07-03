e4a4c2cf31e38eafb26a4aa4c85573ce3861db6c
nhat191024 <richberchannel01@gmail.com>
2024-07-03 17:25:52 +0700

    Create: kitchen service

--- backend/app/Service/KitchenService.php
+++ backend/app/Service/KitchenService.php
@@ -4,20 +4,32 @@ namespace App\Service;
 
 use App\Events\OrderCreate;
 use App\Models\Dish;
+use App\Models\Kitchen;
 
 class KitchenService
 {
-    public function sendNewOrder($dishId, $note, $quantity, $table){
+    public function sendNewOrder($branchId, $dishId, $note, $quantity, $table)
+    {
         $dish = Dish::find($dishId);
         $food = $dish->food;
         $foodCookMethod = $food->dish;
         $cookingMethod = $dish->cookingMethod;
-        $kitchenId = $cookingMethod->kitchen;
         $cookingMethodName = $foodCookMethod->count() > 1 ? " " . $cookingMethod->name : '';
         $dishName = $food->name . $cookingMethodName;
 
+        $kitchen = Kitchen::where("branch_id", $branchId)
+            ->whereHas('cookingMethod', function ($query) use ($cookingMethod) {
+                $query->where('cooking_method_id', $cookingMethod->id);
+            })
+            ->with(['cookingMethod' => function ($query) use ($cookingMethod) {
+                $query->where('cooking_method_id', $cookingMethod->id);
+            }])
+            ->get()
+            ->toArray();
 
-        $kitchen=1;
-        event(new OrderCreate($dishName, $note, $quantity, $table, $kitchen));
+        $kitchenId = $kitchen[0]['id'];
+
+        event(new OrderCreate($dishName, $note, $quantity, $table, $kitchenId));
     }
 }
+

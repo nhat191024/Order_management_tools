499f15e2bbc31853134c416de66118005577e148
nhat191024 <richberchannel01@gmail.com>
2024-07-03 17:15:34 +0700

    Update: add sql query log

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+            DB::listen(function ($query) {
+        Log::info($query->sql, $query->bindings, $query->time);
+    });
     }
 }

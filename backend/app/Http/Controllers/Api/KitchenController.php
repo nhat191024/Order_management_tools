55f2096ec73e0a0fefbeaedb48dda3217a014639
Fumino The Fish <richberchannel01@gmail.com>
2024-07-04 18:13:51 +0700

    Update: complete kitchen api

--- backend/app/Http/Controllers/Api/KitchenController.php
+++ backend/app/Http/Controllers/Api/KitchenController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\Branch;
 use App\Models\Kitchen;
-use App\Http\Controllers\Controller;
+use App\Models\Bill;
 
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BillCollection;
+use App\Models\KitchenCookingMethod;
 use App\Service\KitchenService;
 
 class KitchenController extends Controller
@@ -32,10 +35,7 @@ class KitchenController extends Controller
     public function getKitchenOrders(Request $request)
     {
         $orders = $this->service->getCurrentOrders($request->kitchenId, $request->branchId);
-
-        return  response()->json([
-            'orders' => $orders,
-        ]);
+        return $orders;
     }
 }
 

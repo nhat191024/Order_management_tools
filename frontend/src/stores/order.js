6972e5af1775d5330072f1c288241777dd80ccd2
Fumino The Fish <richberchannel01@gmail.com>
2024-07-04 22:29:15 +0700

    Refactor: Store dishes in local storage for persistence

--- frontend/src/stores/order.js
+++ frontend/src/stores/order.js
@@ -2,14 +2,16 @@ import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useOrderStore = defineStore('order', () => {
-    const dishes = reactive([]);
+    const dishes = reactive(JSON.parse(localStorage.getItem('dishes') || '[]'));
 
     function addDish(dish) {
         dishes.push(dish);
+        localStorage.setItem('dishes', JSON.stringify(dishes));
     }
 
     function clearDishes() {
         dishes.splice(0, dishes.length);
+        localStorage.setItem('dishes', JSON.stringify(dishes));
     }
 
     return {

7bef3156a65368374275de77d138ab95aa509421
Fumino The Fish <richberchannel01@gmail.com>
2024-07-04 18:15:58 +0700

    Update: KitchenService to retrieve current orders for a specific kitchen and branch

--- backend/app/Service/KitchenService.php
+++ backend/app/Service/KitchenService.php
@@ -6,6 +6,7 @@ use App\Events\OrderCreate;
 use App\Models\Bill;
 use App\Models\Dish;
 use App\Models\Kitchen;
+use App\Models\KitchenCookingMethod;
 
 class KitchenService
 {
@@ -33,10 +34,38 @@ class KitchenService
         event(new OrderCreate($dishName, $note, $quantity, $table, $kitchenId));
     }
 
-    public function getCurrentOrders($kitchenId, $branchId){
-        Bill::where('branch_id', $branchId)
-        ->with('billDetail')
-        ;
+    public function getCurrentOrders($kitchenId, $branchId)
+    {
+
+        $dishes = Bill::where('branch_id', $branchId)
+            ->where('pay_status', 0)
+            ->with('billDetail.dish', 'billDetail.dish.food', 'billDetail.dish.cookingMethod')
+            ->get();
+        $kitchen = Kitchen::where('id', $kitchenId)
+            ->where('branch_id', $branchId)
+            ->first();
+
+        if ($dishes->isEmpty() || !$kitchen) {
+            return response()->json(['message' => 'Hiện không có đơn được đặt'], 404);
+        }
+
+        $kitchenCookingMethods = KitchenCookingMethod::where('kitchen_id', $kitchenId)->get();
+
+        $orders = [];
+
+        foreach ($dishes as $dish) {
+            foreach ($dish->billDetail as $billDetail) {
+                $cookingMethodId = $billDetail->dish->cooking_method_id;
+                if (in_array($cookingMethodId, $kitchenCookingMethods->pluck('cooking_method_id')->toArray())) {
+                    $orders[] = [
+                        'dish_name' => $billDetail->dish->food->name . ' ' . $billDetail->dish->cookingMethod->name,
+                        'quantity' => $billDetail->quantity,
+                        'note' => $billDetail->note,
+                        'table' => $dish->table_id,
+                    ];
+                }
+            }
+        }
+        return $orders;
     }
 }
-

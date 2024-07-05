5ec8e6533765f6f488469c60c3880a876da6bd7a
Fumino The Fish <richberchannel01@gmail.com>
2024-07-05 11:29:20 +0700

    Update: foodQueue.vue component to improve order completion functionality

--- frontend/src/pages/staffs/foodQueue.vue
+++ frontend/src/pages/staffs/foodQueue.vue
@@ -19,19 +19,19 @@
     </div>
     <div class="grid gap-4 p-4 mt-20 place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <div v-for="(item, index) in items" :key="index"
-        class="flex flex-col items-center p-4 m-2 text-center text-white bg-primary rounded-lg shadow-lg w-64 relative">
-        <p class="absolute top-0 left-0 mt-2 ml-2 text-xl">{{ index + 1 }}</p>
-        <div class="text-lg font-bold">{{ item.name }}</div>
+        class="flex flex-col items-center p-3 m-2 text-center text-white bg-primary rounded-lg shadow-lg w-64 relative">
+        <div class="text-xl font-bold w-full flex justify-between"><span>{{ index + 1 }}</span><span>Bàn {{ item.table }}</span></div>
+        <div class="w-full border-t border-white"></div>
+        <div class="text-lg font-bold mt-3">{{ item.quantity }}x {{ item.name }}</div>
         <div class="w-full my-2 border-t border-white"></div>
         <div class="text-lg">{{ item.note ? item.note : 'Không có ghi chú' }}</div>
-        <div class="flex items-center justify-between w-full">
-          <div class="w-full my-2 border-t border-white"></div>
-          <div class="flex items-center justify-center min-w-14 h-14 p-2 bg-white rounded-full"
+        <div class="flex items-center justify-end w-full mt-2">
+          <div class="flex items-center justify-center min-w-12 h-12 p-2 bg-white rounded-full"
             @click="completeOrder(item.id)">
             <img src="./../../assets/check.svg" alt="Check Icon" />
           </div>
         </div>
-        <div class="text-xl font-bold">Bàn {{ item.table }}</div>
+
       </div>
     </div>
   </div>

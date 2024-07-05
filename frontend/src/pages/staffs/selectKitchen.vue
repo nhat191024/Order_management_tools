bbe2a5b6ed5e619696029e16766ba556ecbf7407
Fumino The Fish <richberchannel01@gmail.com>
2024-07-05 13:20:43 +0700

    Update: selectKitchen.vue to improve dropdown menu and add logout functionality

--- frontend/src/pages/staffs/selectKitchen.vue
+++ frontend/src/pages/staffs/selectKitchen.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="grid grid-cols-12 grid-rows-8 gap-2 h-dvh w-dvw">
     <div class="col-span-2 flex items-center justify-center">
-      <button class="items-center bg-white drop-shadow-2x text-black inline-flex">
-        <div class="bg-primary w-10 h-10 m-2 drop-shadow-2xl rounded-xl p-1">
-          <img src="./../../assets/left-long-solid.svg" alt="My SVG Icon" />
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="m-1">
+          <img class="bg-white w-16 h-16 mr-2 rounded-full border shadow-lg" src="../../assets/logo.jpg"></img>
         </div>
-        <span class="text-xl drop-shadow">Trở về</span>
-      </button>
+        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+          <li><a>Xin chào {{ username }} !</a></li>
+          <li><button @click="logout">Đăng Xuất</button></li>
+        </ul>
+      </div>
+      <p class="font-bold text-xl">Thương ốc</p>
     </div>
     <div class="col-start-5 col-span-4 self-center mx-auto font-extrabold text-3xl">
       <p>Chọn Bếp</p>
@@ -25,10 +29,16 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
+import { useRouter } from 'vue-router';
+import { logoutHandle } from '../../api/login';
 import { getKitchenData } from "../../api/kitchen";
 import { getCookie } from "../../api/functions";
-const kitchens = ref([]);
+
+const router = useRouter();
+const kitchens = ref([])
 const branchId = getCookie("Branch_id");
+const username = getCookie('Username');
+
 onMounted(async () => {
   try {
     kitchens.value = await getKitchenData(branchId);
@@ -36,4 +46,9 @@ onMounted(async () => {
     console.error("Error fetching kitchen data:", error);
   }
 });
+
+function logout() {
+  logoutHandle();
+  router.push('/staff/login');
+}
 </script>

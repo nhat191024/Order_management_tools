917cbfa160d923233f5b908035e7d300056ecdb0
Fumino The Fish <richberchannel01@gmail.com>
2024-07-07 20:48:31 +0700

    refactor: Update staff router paths and components

--- frontend/src/routers/staff.js
+++ frontend/src/routers/staff.js
@@ -1,39 +1,33 @@
 const staff = [
   {
-    path: "/staff",
-    component: () => import("../layouts/staff.vue"),
-    children: [
-      {
-        path: "/staff/login",
-        name: "login",
-        component: () => import("../pages/staffs/index.vue"),
-      },
-      {
-        path: "/staff/table",
-        name: "table selection",
-        component: () => import("../pages/staffs/selectTable.vue"),
-      },
-      {
-        path: "/staff/table/:id",
-        name: "table detail",
-        component: () => import("../pages/staffs/tableDetail.vue"),
-      },
-      {
-        path: "/staff/kitchen",
-        name: "kitchen selection",
-        component: () => import("../pages/staffs/selectKitchen.vue"),
-      },
-      {
-        path: "/staff/kitchen/:id",
-        name: "waiting food",
-        component: () => import("../pages/staffs/foodQueue.vue"),
-      },
-      {
-        path: "/staff/checkout/:id",
-        name: "checkout",
-        component: () => import("../pages/staffs/checkout.vue"),
-      },
-    ],
+    path: "/staff/login",
+    name: "login",
+    component: () => import("../pages/staffs/index.vue"),
+  },
+  {
+    path: "/staff/table",
+    name: "table selection",
+    component: () => import("../pages/staffs/selectTable.vue"),
+  },
+  {
+    path: "/staff/table/:id",
+    name: "table detail",
+    component: () => import("../pages/staffs/tableDetail.vue"),
+  },
+  {
+    path: "/staff/kitchen",
+    name: "kitchen selection",
+    component: () => import("../pages/staffs/selectKitchen.vue"),
+  },
+  {
+    path: "/staff/kitchen/:id",
+    name: "waiting food",
+    component: () => import("../pages/staffs/foodQueue.vue"),
+  },
+  {
+    path: "/staff/checkout/:id",
+    name: "checkout",
+    component: () => import("../pages/staffs/checkout.vue"),
   },
 ];
 export default staff;

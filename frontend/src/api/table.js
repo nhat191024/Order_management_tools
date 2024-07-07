971c963206514efefb2bb487e0118b931c0bc99e
Fumino The Fish <richberchannel01@gmail.com>
2024-07-08 03:35:35 +0700

    refactor: Improve getTableData function in table.js

--- frontend/src/api/table.js
+++ frontend/src/api/table.js
@@ -1,11 +1,7 @@
 import { api } from './api';
 
 export async function getTableData(id) {
-    try {
-        return await api.get(`staff/table/${id}`).then((res) => {
-            return res.data.data;
-        });
-    } catch (error) {
-        window.location.reload();
-    }
+    return await api.get(`staff/table/${id}`).then((res) => {
+        return res.data.data;
+    });
 }

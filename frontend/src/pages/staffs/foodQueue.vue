c1c136bd326310f634b6d06a7fa38f49fa1b14f7
Fumino The Fish <richberchannel01@gmail.com>
2024-07-10 20:13:09 +0700

    refactor: Add cancel functionality to foodQueue.vue

--- frontend/src/pages/staffs/foodQueue.vue
+++ frontend/src/pages/staffs/foodQueue.vue
@@ -20,13 +20,17 @@
     <div class="grid gap-4 p-4 mt-20 place-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       <div v-for="(item, index) in items"
         class="flex flex-col items-center p-3 m-2 text-center text-white bg-primary rounded-lg shadow-lg w-64 relative">
-        <div class="text-xl font-bold w-full flex justify-between"><span>{{ index+1 }}</span><span>Bàn {{ item.table
+        <div class="text-xl font-bold w-full flex justify-between"><span>{{ index + 1 }}</span><span>Bàn {{ item.table
             }}</span></div>
         <div class="w-full border-t border-white"></div>
         <div class="text-lg font-bold mt-3">{{ item.quantity }}x {{ item.name }}</div>
         <div class="w-full my-2 border-t border-white"></div>
         <div class="text-lg">{{ item.note ? item.note : 'Không có ghi chú' }}</div>
-        <div class="flex items-center justify-end w-full mt-2">
+        <div class="flex items-center gap-5 justify-end w-full mt-2">
+          <div class="flex items-center justify-center min-w-12 h-12 p-2 bg-white rounded-full"
+            @click="cancel(item.id)">
+            <img src="./../../assets/xmark.svg" alt="Check Icon" />
+          </div>
           <div class="flex items-center justify-center min-w-12 h-12 p-2 bg-white rounded-full"
             @click="confirm(item.id)">
             <img src="./../../assets/check.svg" alt="Check Icon" />
@@ -55,12 +59,32 @@
       </div>
     </form>
   </dialog>
+
+  <dialog id="cancel" class="modal">
+    <form class="modal-box text-center">
+      <h3 class="font-bold text-lg text-orange-500">Xác nhận hủy món?</h3>
+      <p class="py-4">
+        Hãy chắc chắn rằng bạn đã kiểm tra kỹ trước khi nhấn xác nhận!
+      </p>
+      <p class="py-4 font-bold">
+        Hệ thống sẽ tiến hành hủy món khi bạn xác nhận!
+      </p>
+      <div class="modal-action border-t border-black pt-4 mt-0">
+        <form method="dialog">
+          <button class="btn btn-primary text-white mx-2" @click="cancelOrder()">
+            Xác Nhận
+          </button>
+          <button class="btn btn-error text-white">Hủy</button>
+        </form>
+      </div>
+    </form>
+  </dialog>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
-import { getKitchenCurrentOrder, updateOrderStatus, getKitchenName } from "../../api/kitchen";
+import { getKitchenCurrentOrder, updateOrderStatus, getKitchenName, orderDelete } from "../../api/kitchen";
 import { getCookie, checkLogin } from "../../api/functions";
 
 checkLogin();
@@ -79,7 +103,6 @@ onMounted(() => {
 
 async function getCurrentOrder() {
   getKitchenCurrentOrder(branchId, id).then((res) => {
-    // items.value.push(...res);
     items.value = res;
   });
 }
@@ -97,8 +120,24 @@ function confirm(id) {
   orderId.value = id;
 }
 
+function cancel(id) {
+  const confirm = document.getElementById('cancel')
+  confirm.showModal();
+  orderId.value = id;
+}
+
 async function completeOrder() {
   updateOrderStatus(orderId.value).then((res) => {
+    if (res.message === 'success') {
+      getCurrentOrder();
+    } else {
+      return;
+    }
+  });
+}
+
+async function cancelOrder() {
+  orderDelete(orderId.value).then((res) => {
     if (res.message === 'success') {
       getCurrentOrder();
     } else {
